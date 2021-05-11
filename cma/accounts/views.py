64f47900323fb40bbe6b66ddc63c5151c71a5d1f
Prajol Sethi <prajolsethi@gmail.com>
2021-05-11 23:11:13 +0530

    PART 8 : rrendering django module

--- cma/accounts/views.py
+++ cma/accounts/views.py
@@ -1,12 +1,21 @@
 from django.shortcuts import render
 # from django.http import HttpResponse
 # Create your views here.
-
+from .models import *
 def home(request):
-    return render(request,'accounts/dashboard.html')
+    customers = Customer.objects.all()
+    orders = Order.objects.all()
+    total_orders = orders.count()
+    orders_delivered = orders.filter(status="delivered").count()
+    orders_pending = orders.filter(status="pending").count()
+    context = {'customers':customers,'orders':orders,
+    'total_orders':total_orders,'orders_delivered':orders_delivered,'orders_pending':orders_pending}
+    return render(request,'accounts/dashboard.html',context)
 
 def product(request):
-    return render(request,'accounts/products.html')
+    product = Product.objects.all()
+    return render(request,'accounts/products.html', {'product':product})
 
 def customer(request):
-    return render(request,'accounts/customer.html')
+    # customer= Customer.objects.all()
+    return render(request,'accounts/customer.html',{'customer':customer})
